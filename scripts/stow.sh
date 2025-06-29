d296d4bf6982553916f4cd56418e439717f158d6
M0sT4fa <mostafa2018a3s@gmail.com>
2025-06-29 17:40:51 +0200

    refactor: make stowing more modular

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -2,26 +2,28 @@
 set -euo pipefail
 
 # DOTFILES_DIR="$HOME/dotfiles"
-TARGET_DIR="$HOME"
-ROOT_TARGET_DIR="/"
 
-CONFIGS=()
-parse_array_file "$DEPS_DIR/stow.txt" CONFIGS
+declare -A CONFIGS
+parse_kv_file "$DEPS_DIR/stow.txt" CONFIGS
 
 info "Changing directory to dotfiles repository: $DOTFILES_DIR."
 cd "$DOTFILES_DIR" || { error "Dotfiles directory not found!"; exit 1; }
 
 info "Stowing dotfiles..."
-for cfg in "${CONFIGS[@]}"; do
+for cfg in "${!CONFIGS[@]}"; do
 
-  if [ "$cfg" = "wallpapers" ]; then
-    info "Stowing $cfg..."
-    sudo stow -v -t "$ROOT_TARGET_DIR" "$cfg"
-  elif [ -d "$cfg" ]; then
-    info "Stowing $cfg..."
-    stow -v -t "$TARGET_DIR" "$cfg"
+  TARGET_DIR=$(eval echo "${CONFIGS[$cfg]}")
+
+  if [[ -d "$cfg" ]]; then
+    if [[ "$TARGET_DIR" == /usr/* || "$TARGET_DIR" == /etc/* ]]; then
+      info "Stowing $cfg to $TARGET_DIR (sudo)..."
+      sudo stow -v -t "$TARGET_DIR" "$cfg"
+    else
+      info "Stowing $cfg to $TARGET_DIR..."
+      stow -v -t "$TARGET_DIR" "$cfg"
+    fi
   else
-    warn "Config $cfg not found!"
+    warn "Config '$cfg' not found — skipping."
   fi
 
 done
